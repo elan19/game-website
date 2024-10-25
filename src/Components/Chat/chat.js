cc0da807b7e68a43eb10277dd4c222a589651e9f
elan19 <elan19@github.com>
2024-10-25 15:55:12 +0200

    Websockets

--- src/Components/Chat/chat.js
+++ src/Components/Chat/chat.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import { UserContext } from '../../util/UserContext';
 
 // Connect to the Socket.IO server
-const socket = io('https://gamipo.org', {
+const socket = io('/', {
     transports: ['websocket'], // Use websockets if available
     withCredentials: true // Include credentials if your server supports them
 });
